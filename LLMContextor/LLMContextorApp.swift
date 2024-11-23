d157ff225840f66eac73ba11ea46563412ab8c26
Rudrank Riyam <rudrankriyam@gmail.com>
2024-11-23 17:34:22 +0530

    Add project

---
+++ LLMContextor/LLMContextorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMContextorApp.swift
+//  LLMContextor
+//
+//  Created by Rudrank Riyam on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct LLMContextorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
